b2d0ef094a0f42111ed49e553f81a1a44a74d2a5
mmiah00 <mmiah00@stuy.edu>
2019-10-16 23:51:15 +0100

    wrote headers, working on returning a sample fo the struct

--- structs.c
+++ structs.c
@@ -1,3 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "structs.h"
+
+struct house sampleHouse ( ) {
+  struct house h;
+  char[5][50] ads = {"35 Elm Street", "123 Arthur Avenue", "80 East 92nd Street", "20 Union Square", "90 Broad Street"};
+  int[5] prices = {200000, 45000, 250000, 275000, 90000};
+  int[5] rooms = {4, 2, 5, 3, 2};
+  int r = rand () % 5;
+  if (r == 0) {
+    h.address = ads [0];
+    h.cost = prices [0];
+    h.numrooms = rooms [0];
+  }
+  if (r == 1) {
+    h.address = ads [0];
+    h.cost = prices [0];
+    h.numrooms = rooms [0];
+  }
+  return h;
+}
+struct boardGame sampleGame ();
+
+void printHouse (struct house h);
+void printGame(struct boardGame g);
+
+void modifyHouse (struct house h, char* ad, int price, int rooms);
+void modifyGame (struct boardGame g, char* title, int players, int pts);
--- structs.h
+++ structs.h
@@ -7,5 +7,14 @@ struct house {
 struct boardGame {
   char[] name;
   int numplayers;
-  int points; 
+  int points;
 };
+
+struct house sampleHouse ( );
+struct boardGame sampleGame ();
+
+void printHouse (struct house h);
+void printGame(struct boardGame g);
+
+void modifyHouse (struct house h, char* ad, int price, int rooms);
+void modifyGame (struct boardGame g, char* title, int players, int pts);
