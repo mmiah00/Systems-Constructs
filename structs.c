7871600120cf4e924c5ec85d3ef23a19aa0e4c12
mmiah00 <mmiah00@stuy.edu>
2019-10-17 00:02:33 +0100

    finished first 2

--- structs.c
+++ structs.c
@@ -14,13 +14,55 @@ struct house sampleHouse ( ) {
     h.numrooms = rooms [0];
   }
   if (r == 1) {
-    h.address = ads [0];
-    h.cost = prices [0];
-    h.numrooms = rooms [0];
+    h.address = ads [1];
+    h.cost = prices [1];
+    h.numrooms = rooms [1];
+  }
+  if (r == 2) {
+    h.address = ads [2];
+    h.cost = prices [2];
+    h.numrooms = rooms [2];
+  }
+  if (r == 3) {
+    h.address = ads [3];
+    h.cost = prices [3];
+    h.numrooms = rooms [3];
+  }
+  else {
+    h.address = ads [4];
+    h.cost = prices [4];
+    h.numrooms = rooms [4];
   }
   return h;
 }
-struct boardGame sampleGame ();
+struct boardGame sampleGame () {
+  struct boardGame g;
+  char [5][50] titles = {"Scrabble", "Chess", "Checkers", "Battleship", "Connect 4"}
+  int [5] players = {6, 2, 2, 4, 4};
+  int r = rand () % 5;
+  if (r == 0) {
+    g.name = titles [0];
+    g.numplayers = players[0];
+  }
+  if (r == 1) {
+    g.name = titles [1];
+    g.numplayers = players[1];
+  }
+  if (r == 2) {
+    g.name = titles [2];
+    g.numplayers = players[2];
+  }
+  if (r == 3) {
+    g.name = titles [3];
+    g.numplayers = players[3];
+  }
+  else {
+    g.name = titles [4];
+    g.numplayers = players[4]; 
+  }
+  g.points = 0;
+  return g;
+}
 
 void printHouse (struct house h);
 void printGame(struct boardGame g);
